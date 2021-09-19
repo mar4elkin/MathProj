8fcca63d3c20d65b19f3e8b0a8058853faac1a21
Mark <googlcraftlol@gmail.com>
2021-09-20 02:29:23 +0300

    removed react

--- frontend-react/src/Components/Auth/SocialNet.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class SocialNet extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-}
--- frontend-react/src/constants.js
+++
@@ -1,6 +0,0 @@
-const projectConstants = {
-  backend_url: "http://127.0.0.1:8000/api/v1"
-}
-
-
-export default projectConstants
